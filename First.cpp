68ec6110c6f751ed795fb9b6bcb217deea15f909
danial324 <danialtahir321@gmail.com>
2022-06-04 10:21:29 +0500

    My Updated List

--- First.cpp
+++ First.cpp
@@ -3,6 +3,10 @@ using namespace std;
 int main()
 {
     string s = "Danial";
+    int dani[5]={1,2,3,4,5};
+    cout<<dani[2]<<endl;
     cout<<s<<endl<<"I have a problem"<<endl;
+//we have to take break 
+
     return 0;
 }
